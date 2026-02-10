bf486846844e56f37ad7d4233ea423dce9f15c48
lomzerd <110047640+lomzerd@users.noreply.github.com>
2026-02-11 07:09:51 +0800

    merge with ysaac branch (#2)
    * generators
    
    * quick push for util merge
    
    bruh
    
    * factions + turns
    
    added barebones factions with factionData sccriptable objects & a turn manager which flipflops between the player turn & the rest of the world's turn
    
    * plannet name and resoruce generation
    
    * removed dih sucking from the debug logs
    
    * planet factions generation
    
    * changed turn manager to work with the new planets
    
    not sure how factions were supposed to work back then but now the turn manager just uses the planet's factions
    
    ---------
    
    Co-authored-by: zen20031566 <tanzenyoung1@gmail.com>

--- Assets/Scripts/Planet/GalaxyGenerator.cs
+++ Assets/Scripts/Planet/GalaxyGenerator.cs
@@ -1,33 +1,7 @@
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using UnityEngine;
 
-
-public class GalaxyGenerator : MonoBehaviour
+public static class GalaxyGenerator 
 {
-    public string GalaxyName { get; private set; }
-    public int SeedInt { get; private set; }
-
-    public System.Random PlanetNameRNG { get; private set; }
-
-    public List<string> GeneratedPlanetNames = new List<string>();
-
-    static readonly string[] planetBaseNames =
-    {
-        "Aurelia",
-        "Nyx",
-        "Helios",
-        "Virex",
-        "Eldara",
-        "Kryos",
-        "Zentha",
-        "Orion",
-        "Caldera",
-        "Solara",
-        "Iris",
-        "Atlas"
-    }; 
-
     static readonly string[] galaxyBaseNames =
     {
         "Andromeda",
@@ -48,39 +22,10 @@ public class GalaxyGenerator : MonoBehaviour
         "Ionis",
     };
 
-    static readonly string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    private void Start()
-    {
-        GalaxyName = GenerateGalaxyName();
-        SeedInt = StringToHashCode(GalaxyName);
-
-        Debug.Log($"Generated Galaxy Name: {GalaxyName} with Seed: {SeedInt}");
-
-        PlanetNameRNG = new System.Random(SeedInt);
-
-    }
-
-    // Changes strings into a consistent hash code integer
-    private int StringToHashCode(string seedStr)
-    {
-        unchecked
-        {
-            long hash = 5381L; //initial prime (Bernstein's djb2)
-            foreach (char c in seedStr)
-                hash = ((hash << 5) + hash) + c; //Equivalent to 33 * hash + c
-            return (int)(hash & 0x7FFFFFFF);
-        }
-    }
-
-    bool IsValidSeed(string seed)
-    {
-        return Regex.IsMatch(seed, @"^[A-Za-z]{6}\d{4}$");
-
-    }
+    public static readonly string SuffixChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     // We use galaxy names as a seed to generate the galaxy which determines the map layout and the planets and other things generated
-    private string GenerateGalaxyName()
+    public static string GenerateGalaxyName()
     {
         System.Random rng = new System.Random();
 
@@ -88,34 +33,37 @@ public class GalaxyGenerator : MonoBehaviour
 
         char[] suffix = new char[3];
         for (int i = 0; i < suffix.Length; i++)
-            suffix[i] = chars[rng.Next(chars.Length)];
+            suffix[i] = SuffixChar[rng.Next(SuffixChar.Length)];
         return baseName + "-" + new string(suffix);
     }
 
-    // We use planet names as a seed to generate aa unique planet visual/ resource/ etc
-    private string GeneratePlanetName()
+    public static Galaxy GenerateGalaxy(string galaxyName, int planetAmount, System.Random rng)
     {
-        string baseName = planetBaseNames[PlanetNameRNG.Next(planetBaseNames.Length)];
-        char[] suffix = new char[4];
-        for (int i = 0; i < suffix.Length; i++)
-            suffix[i] = chars[PlanetNameRNG.Next(chars.Length)];
-        return baseName + "-" + new string(suffix);
+        List<Planet> planets = new List<Planet>();
+
+        for (int i = 0; i < planetAmount; i++)
+        {
+            Planet planet = PlanetGenerator.GeneratePlanet(rng);
+            planets.Add(planet);
+        }
+
+        return new Galaxy(galaxyName, planets);
     }
 
 
-    // Debug
-    [ContextMenu("Generate galaxy name")]
-    private void PrintGeneratedGalaxyName()
-    {
-        string planetName = GenerateGalaxyName();
-        Debug.Log(planetName);
-    }
 
-    [ContextMenu("Generate planet name")]
-    private void PrintGeneratedPlanetName()
-    {
-        string planetName = GeneratePlanetName();   
-        Debug.Log(planetName);
-    }
+    //// Debug
+    //[ContextMenu("Generate galaxy name")]
+    //private void PrintGeneratedGalaxyName()
+    //{
+    //    string planetName = GenerateGalaxyName();
+    //    Debug.Log(planetName);
+    //}
 
+    //[ContextMenu("Generate planet name")]
+    //private void PrintGeneratedPlanetName()
+    //{
+    //    string planetName = GeneratePlanetName();   
+    //    Debug.Log(planetName);
+    //}
 }
--- Assets/Scripts/Planet/GameManager.cs
+++ Assets/Scripts/Planet/GameManager.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int planetAmount = 50;
+
+    public Galaxy CurrentGalaxy { get; private set; }
+    public int SeedInt { get; private set; }
+
+    public System.Random PlanetRNG { get; private set; } // Maybe il change to data struct which combines all rng needed later -Zen
+
+    [SerializeField] private List<Planet> allPlanets = new List<Planet>();
+    public List<Planet> PlanetsWithFactions { get; private set; } = new List<Planet>();
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+
+        if (TurnManager.Instance == null)
+        {
+            GameObject tmObj = new GameObject("TurnManager");
+            tmObj.AddComponent<TurnManager>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // First generate galaxy name which is the seed for everything
+        string galaxyName = GalaxyGenerator.GenerateGalaxyName();
+        SeedInt = SeedUtil.StringToHashCode(galaxyName);
+
+        // Then using the galaxy seed to generate seeds for the things in game ask me if need clarity -Zen
+        PlanetRNG = new System.Random(SeedInt);
+        // MoonNameRNG etc...
+        // Basically anything that needs a seed for generation in game should use this pattern
+
+        // Then generate the galaxy
+        CurrentGalaxy = GalaxyGenerator.GenerateGalaxy(galaxyName, planetAmount, PlanetRNG);
+
+        Debug.Log($"Generated Galaxy Name: {CurrentGalaxy.GalaxyName} with Seed: {SeedInt}");
+
+        allPlanets = CurrentGalaxy.Planets;
+        PlanetsWithFactions = allPlanets.FindAll(planet => planet.FactionType != FactionType.Nothing);
+
+        Debug.Log("Planets in Galaxy:");
+        foreach (var planet in CurrentGalaxy.Planets)
+        {
+            // Join all resources with comma
+            string resources = string.Join(", ", planet.Resources.Keys);
+
+            // Print planet name, resources, and extra newline
+            Debug.Log($"{planet.PlanetName}\n{resources}\n{planet.FactionType}\n");
+
+        }
     }
+
 }
