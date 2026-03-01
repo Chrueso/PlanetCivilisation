90da8d50186895ad83ff6c6bbe31415dcede4b94
lomzerd <ysaaclb@gmail.com>
2026-03-01 22:11:40 +0800

    turn stuff & faction stuff wip
    Trying to make Round Robin styled turns & edited the Factions class to include some other data

--- Assets/Scripts/Faction/Faction.cs
+++ Assets/Scripts/Faction/Faction.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,24 +9,59 @@ public class Faction
     public Dictionary<ShipType, int> shipCounts = new Dictionary<ShipType, int>();
     public List<PlanetData> ownedPlanets = new List<PlanetData>();
 
+    //added by ysaac
+    public bool IsPlayer { get; private set; }
+    public int ActionPoints { get; private set; }
+    private int baseActionPoints;
+    private int maxActionPoints;
+
+    public event System.Action OnTurnFinished;
+
     public Faction(FactionDataSO factionData)
     {
         FactionType = factionData.FactionType;
         Money = factionData.Money;
+        IsPlayer = factionData.IsPlayer;
+
+        baseActionPoints = factionData.BaseActionPoints;
+        maxActionPoints = baseActionPoints;
+        ActionPoints = maxActionPoints;
+
         foreach (var shipData in factionData.startingShipDatas)
         {
             shipCounts[shipData.ShipType] = shipData.Amount;
         }
     }
 
-    public virtual void StartWorldTurn()
+    public virtual void StartPlayerTurn()
     {
-        Debug.Log($"{FactionType} is doing stuff");
-        EndWorldTurn();
+        Debug.Log($"{FactionType} is beginning");
+        //ActionPoints = maxActionPoints;
+        // Enable player controls here
+        RunAiTurn();
     }
 
-    public virtual void EndWorldTurn()
+    public virtual void StartAITurn()
+    {
+        Debug.Log($"{FactionType} is beginning");
+        //ActionPoints = maxActionPoints;
+        RunAiTurn();
+    }
+
+    public virtual void RunAiTurn()
+    {
+        while(ActionPoints > 0)
+        {
+            //do ai stuff here
+            ActionPoints--;
+            Debug.Log($"{FactionType} did an action");
+        }
+        TurnManager.Instance.EndTurn();
+    }
+
+    public virtual void EndTurn()
     {
         Debug.Log($"{FactionType} has finished doing stuff");
+        OnTurnFinished?.Invoke();
     }
 }
