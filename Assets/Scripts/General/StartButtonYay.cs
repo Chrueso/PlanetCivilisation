68057a13119445e36dc48b325ee498e59184a624
Chrueso <81568143+Chrueso@users.noreply.github.com>
2026-05-08 12:00:07 +0800

    final build

--- Assets/Scripts/General/StartButtonYay.cs
+++ Assets/Scripts/General/StartButtonYay.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 public class StartButtonYay : MonoBehaviour
 {
     [SerializeField] private Button startButton;
+    [SerializeField] private Button creditButton;
+    [SerializeField] private GameObject credit;
+    [SerializeField] private Button closeCredit;
     [SerializeField] private AudioLibrary audioLib;
     [SerializeField] private AudioSystem audioSystem;
 
@@ -12,16 +15,31 @@ public class StartButtonYay : MonoBehaviour
     private void OnEnable()
     {
         startButton.onClick.AddListener(Yay);
+        creditButton.onClick.AddListener(OpenCredits);
+        closeCredit.onClick.AddListener(CloseCredits);
         audioSystem.PlayMusic(audioLib.mainMenuMusic);
     }
 
     private void OnDisable()
     {
         startButton.onClick.RemoveListener(Yay);
+        creditButton.onClick.RemoveListener(OpenCredits);
+        closeCredit.onClick.RemoveListener(CloseCredits);
     }
 
     private void Yay()
     {
         SceneManager.LoadScene("MainGameScene");
     }
+
+    private void OpenCredits()
+    {
+        credit.SetActive(true);
+    }
+
+    private void CloseCredits()
+    {
+        credit.SetActive(false);
+    }
+
 }
